6862d5bc730fa95a9f78d923717fe97c1b05823a
DDemmer1 <Dennis.Demmer@web.de>
2020-05-18 19:47:05 +0200

    add more hints

--- src/de/demmer/dennis/Sudoku.java
+++ src/de/demmer/dennis/Sudoku.java
@@ -4,7 +4,31 @@ public class Sudoku {
 
 	public boolean isValid(int[][] grid) {
 		
+//		Jede Reihe 1-9 
+//		Wenn nicht: 
+//		return false;
 		
+//		Jede Zeile 1-9
+//		Wenn nicht: 
+//		return false;
+		
+		
+//		Jede 3x3 box die Zahlen 1 bis 9
+//		Wenn nicht: 
+//		return false;
+		
+		
+		
+		return true;
+	}
+	
+	
+	
+	
+	private boolean is1To9(int[] list) {
+//		Überprüft ob das 1D Array 1-9 enhält
+//		Wenn nicht: 
+//		return false;
 		
 		return true;
 	}
